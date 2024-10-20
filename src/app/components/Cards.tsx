3a78d9d2b962da08ca26ddcc0452ef4efa87a971
Hanif8193 <hanifdostmuhammad8193@gmail.com>
2024-10-20 11:48:24 +0500

    useing-props

---
+++ src/app/components/Cards.tsx
@@ -0,0 +1,12 @@
+
+import React from 'react'
+
+function Cards (prop:any) {
+  return (
+            <div className=' flex justify-center items-center p-4  bg-green-500 rounded-md ' >
+          {prop.name}
+          
+        </div>
+      )}
+
+      export default Cards
